93249ee3726d712b1aa4c5f27164c150115a14a4
F3700 <albertfebrian26@gmail.com>
2025-04-13 22:22:17 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace tpmodul8_103022300003
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public string batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        private const string FilePath = "../../../covid_config.json";
+
+        public void ReadJSON()
+        {
+            string json = File.ReadAllText(FilePath);
+            var config = JsonSerializer.Deserialize<CovidConfig>(json);
+
+            satuan_suhu = config.satuan_suhu == "CONFIG1" ? "celcius" : config.satuan_suhu;
+            batas_hari_deman = config.batas_hari_deman.ToString() == "CONFIG2" ? "14" : config.batas_hari_deman;
+            pesan_ditolak = config.pesan_ditolak == "CONFIG3" ? "Anda tidak diperbolehkan masuk ke dalam gedung ini" : config.pesan_ditolak;
+            pesan_diterima = config.pesan_diterima == "CONFIG4" ? "Anda dipersilahkan untuk masuk ke dalam gedung ini" : config.pesan_diterima;
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu.ToLower() == "celcius")
+                satuan_suhu = "fahrenheit";
+            else
+                satuan_suhu = "celcius";
+
+            Save();
+        }
+
+        private void Save()
+        {
+            string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePath, json);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,86 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul8_103022300003;
+
+CovidConfig covidConfig = new CovidConfig();
+covidConfig.ReadJSON();
+double suhu;
+int hari;
+
+//Dalam Celcius
+Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {covidConfig.satuan_suhu}");
+suhu = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+hari = int.Parse(Console.ReadLine());
+
+
+if (int.Parse(covidConfig.batas_hari_deman) > hari )
+{
+    if (covidConfig.satuan_suhu == "celcius")
+    {
+        if (suhu >= 36.5 && suhu <= 37.5)
+        {
+            Console.WriteLine(covidConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(covidConfig.pesan_ditolak);
+        }
+    }
+    else if(covidConfig.satuan_suhu == "fahrenheit")
+    {
+        if (suhu >= 97.5 && suhu <= 99.5)
+        {
+            Console.WriteLine(covidConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(covidConfig.pesan_ditolak);
+        }
+    }
+}
+else
+{
+    Console.WriteLine(covidConfig.pesan_ditolak);
+}
+Console.WriteLine("\n");
+
+//Dalam Fahrenheit
+covidConfig.UbahSatuan();
+
+Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {covidConfig.satuan_suhu}");
+suhu = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+hari = int.Parse(Console.ReadLine());
+
+if (int.Parse(covidConfig.batas_hari_deman) > hari)
+{
+    if (covidConfig.satuan_suhu == "celcius")
+    {
+        if (suhu >= 36.5 && suhu <= 37.5)
+        {
+            Console.WriteLine(covidConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(covidConfig.pesan_ditolak);
+        }
+    }
+    else if (covidConfig.satuan_suhu == "fahrenheit")
+    {
+        if (suhu >= 97.5 && suhu <= 99.5)
+        {
+            Console.WriteLine(covidConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(covidConfig.pesan_ditolak);
+        }
+    }
+}
+else
+{
+    Console.WriteLine(covidConfig.pesan_ditolak);
+}
